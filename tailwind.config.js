6fd87426f4cbf8542fd2aa396707697ab47225a2
Yamato <siescollege04@gmail.com>
2026-02-12 20:31:22 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                gold: {
+                    500: '#D4AF37',
+                    600: '#C5A028',
+                },
+                royalBlue: {
+                    900: '#0F172A',
+                },
+            },
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+}
